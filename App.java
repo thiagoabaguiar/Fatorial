69f417fc4b80f02e4ee5b0aaaa715678cc218211
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-03-07 22:23:58 -0300

    Update files

---
+++ App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args){
+
+        int fatorial;
+
+        System.out.println("Deseja obter o Fatorial de qual número?");
+        try (Scanner input = new Scanner(System.in)) {
+
+       
+
+            System.out.println("O resultado final é " + result);
+
+        }
+
+    }
+
+}
