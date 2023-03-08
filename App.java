75800e01c89e087303a6da87967a5fdd4da1629f
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-03-08 15:03:13 -0300

    Update loop

--- App.java
+++ App.java
@@ -4,12 +4,22 @@ public class App {
 
     public static void main(String[] args){
 
-        int fatorial;
+        int factorial;
+        int result;
 
         System.out.println("Deseja obter o Fatorial de qual número?");
         try (Scanner input = new Scanner(System.in)) {
 
-       
+            factorial = input.nextInt();
+            result = factorial;
+
+            for (int i = (factorial -1); i <= 1; i--){
+
+                result = result * i;
+                System.out.println("resultado: " + result);
+                System.out.println("contador: " + i);
+
+            };       
 
             System.out.println("O resultado final é " + result);
 
